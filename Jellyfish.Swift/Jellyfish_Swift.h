246787a7f62f2d7531470bf110dd1f30eaf3de62
Marc Rousavy <marcrousavy@hotmail.com>
2018-10-17 15:06:25 +0200

    Add Project

---
+++ Jellyfish.Swift/Jellyfish_Swift.h
@@ -0,0 +1,19 @@
+//
+//  Jellyfish_Swift.h
+//  Jellyfish.Swift
+//
+//  Created by Marc Rousavy on 17.10.18.
+//  Copyright © 2018 mrousavy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Jellyfish_Swift.
+FOUNDATION_EXPORT double Jellyfish_SwiftVersionNumber;
+
+//! Project version string for Jellyfish_Swift.
+FOUNDATION_EXPORT const unsigned char Jellyfish_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Jellyfish_Swift/PublicHeader.h>
+
+
---
+++ Jellyfish.SwiftTests/Jellyfish_SwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jellyfish_SwiftTests.swift
+//  Jellyfish.SwiftTests
+//
+//  Created by Marc Rousavy on 17.10.18.
+//  Copyright © 2018 mrousavy. All rights reserved.
+//
+
+import XCTest
+@testable import Jellyfish_Swift
+
+class Jellyfish_SwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
